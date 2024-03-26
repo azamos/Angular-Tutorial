15d047a8deb16faa828f5db2bb0e5c3d20a46689
amos zohar <azamoszohar@gmail.com>
2024-03-26 12:31:32 +0200

    added fake server interaction

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,8 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError,map,tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +13,75 @@ export class HeroService {
   // getHeroes(): Hero[] {
   //   return HEROES;
   // }
+  // getHeroes(): Observable<Hero[]> {
+  //   const heroes = of(HEROES);/* async call */
+  //   this.messageService.add('HeroService: fetched heroes');
+  //   return heroes;
+  // }
+  // getHero(id:number) {
+  //   const hero = HEROES.find(h=>h.id === id);
+  //   this.messageService.add(`HeroService: fetched hero id=${id}`);
+  //   return of(hero);
+  // }
+  // constructor(private messageService: MessageService){}
+  constructor(private http: HttpClient,private messageService: MessageService){}
+
+  private log(message:string){
+    this.messageService.add(`HeroService: ${message}`);
+  }
+  //                 :base/:collectionName
+  private heroesUrl = 'api/heroes';
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);/* async call */
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    /* even though the response indicates multiple Hero[] arrays, only one is fetched */
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_=>this.log('fetched heroes'))
+        ,catchError(this.handleError<Hero[]>('getHeroes',[])));
   }
-  getHero(id:number) {
-    const hero = HEROES.find(h=>h.id === id);
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+  /**
+   * 
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   * @returns - empty result
+   */
+  private handleError<T>(operation='operation',result?:T){
+    return (error:any):Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      /*Let the app keep running by returning an empty result */
+      return of(result as T);
+    }
   }
-  constructor(private messageService: MessageService){}
+  /** GET hero by id. Will 404 if id not found */
+  getHero(id:number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url)
+      .pipe(
+        tap(_=>this.log(`HeroService: feetched hero id = ${id}`)),
+        catchError(this.handleError<Hero>(`getHero id=${id}`)));
+  }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  updateHero(hero:Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+        tap(_=>this.log(`updated hero id=${hero.id}`)),
+        catchError(this.handleError<any>('updateHero'))
+      );
+  }
+  addHero(hero:Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl,hero,this.httpOptions)
+            .pipe(
+              tap((newHero:Hero)=> this.log(`added hero w/ id=${newHero.id}`)),
+              catchError(this.handleError<Hero>('addHero'))
+            );
+  }
+  deleteHero(id:number):Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -28,4 +28,21 @@ export class HeroesComponent implements OnInit {
     and susbscribe reminds of .then(res=>console.log(res)) */
     this.heroService.getHeroes().subscribe(res => this.heroes = res);
   }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+  delete(hero:Hero):void {
+    this.heroes = this.heroes.filter(h=>h!==hero);
+    /*It must subscribe anyway, even though the component has nothing to do with the Observable
+    returned by deleteHero from HeroService*/
+    this.heroService.deleteHero(hero.id).subscribe();
+    /*An observable does nothing until we call subscribe. Thus, if we remove the
+    subscribe call, locally it would appear as though the hero was deleted,
+    in reality,it was not actually deleted from the server's DB. */
+  }
 }
